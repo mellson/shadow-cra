58769d149f100d69a65d468fda260ae28bf788f8
Anders Bech Mellson <mellson@icloud.com>
2023-09-28 08:27:02 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import browserSandbox from '@exact-realty/lot';
 import { createMachine } from 'xstate';
 import './App.css';
-import logo from './logo.svg';
 
 async function goddag(navn) {
   return `hejsa ${navn}`;
@@ -44,18 +43,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <iframe
           id="sandboxFrame"
           sandbox="allow-scripts"
