aad57843eaba1845d39c4fac518581dfd655865f
Anders Bech Mellson <mellson@icloud.com>
2023-09-28 08:11:05 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
-import logo from './logo.svg';
+import browserSandbox from '@exact-realty/lot';
+import { createMachine } from 'xstate';
 import './App.css';
+import logo from './logo.svg';
+
+async function goddag(navn) {
+  return `hejsa ${navn}`;
+}
+
+const externalMethods = {
+  createMachine,
+  goddag,
+};
+const sandbox = await browserSandbox(
+  `
+  /* sandboxed code*/;
+  module.exports={
+    hello:async (navn)=>\`Hello \${await goddag(navn)}!\`,
+    getMachine:async (config)=>{
+      // fetch('https://jsonplaceholder.typicode.com/todos/1');
+      const machine = await createMachine(config);
+      console.log(machine.id);
+      return machine.id;
+    },
+  };
+`,
+  ['fetch', 'createMachine'],
+  externalMethods,
+  undefined,
+  { browserRequireWorker: true }
+);
+const result = await sandbox('hello', 'Anders');
+console.log(result);
+
+const miniMachineConfig = {
+  id: 'mini',
+  initial: 'idle',
+  states: { idle: {} },
+};
+const machine = await sandbox('getMachine', miniMachineConfig);
+// console.log(machine);
 
 function App() {
   return (
@@ -17,6 +56,16 @@ function App() {
         >
           Learn React
         </a>
+        <iframe
+          id="sandboxFrame"
+          sandbox="allow-scripts"
+          srcDoc="
+          <script>
+          // Your JavaScript code
+          console.log('Hello, Sandbox!');
+          </script>
+          "
+        ></iframe>
       </header>
     </div>
   );
