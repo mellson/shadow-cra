eb43b86f42f4c95738a26f50ee053c2637440236
Anders Bech Mellson <mellson@icloud.com>
2023-09-30 14:57:05 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import { createMachine } from 'xstate';
 import './App.css';
 
+window.hello = 'Hello, World!';
+
 const miniMachineConfig = {
+  predictableActionArguments: true,
   id: 'mini',
   initial: 'idle',
   states: { idle: {} },
 };
-const machine = createMachine(miniMachineConfig);
+
+function iframeContent(config) {
+  return `
+  <script type="module">
+    import { createMachine } from 'https://cdn.skypack.dev/xstate';
+    alert('Hello, World!');
+    console.log(window.hello); 
+    const machine = createMachine(${config});
+    console.log(machine.id);
+  </script>
+  `;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <iframe
-          id="sandboxFrame"
           sandbox="allow-scripts"
-          srcDoc="
-          <script>
-          // Your JavaScript code
-          console.log('Hello, Sandbox!');
-          </script>
-          "
+          title="sandbox"
+          srcDoc={iframeContent(JSON.stringify(miniMachineConfig))}
+          style={{ display: 'none', position: 'absolute' }}
         ></iframe>
       </header>
     </div>
